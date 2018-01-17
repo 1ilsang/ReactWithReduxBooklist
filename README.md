11c27ca45626de3f31dd2793150d14339ef9460e
Jackson Lee <1ilsang@naver.com>
2018-01-17 09:09:29 +0900

    booklist

---
+++ README.md
@@ -0,0 +1,7 @@
+# ReactWithReduxBooklist
+
+리덕스 연습용 프로젝트.
+
+Store에 booklist를 두고 마우스 클릭시 action.
+
+##with ReduxSimpleStarter
---
+++ src/reducers/reducer_books.js
@@ -0,0 +1,8 @@
+export default function() {
+    return [
+        {title: 'Javascript', pages: 101},
+        {title: 'Harry Potter', pages: 111},
+        {title: 'The Dark Tower', pages: 202},
+        {title: 'IU', pages: 455}
+    ]
+}
